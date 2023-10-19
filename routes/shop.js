5deca9d9aaeb7bc627a738da0802a51652278ab0
Ivo Nunes <inunes1904@gmail.com>
2023-10-19 21:08:27 +0100

    Express Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,13 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   // res.sendFile(path.join(rootDir, 'views', 'shop.html')); normal one
-  res.render('shop', {prods : products, docTitle:'Shop', path:"/", pageTitle:"Shop"})
+  res.render('shop', 
+  {prods : products, 
+    docTitle:'Shop', 
+    path:"/",
+    pageTitle:"Shop",
+    activeShop: true,
+    productCSS:true });
 });
 
 module.exports = router;
